bce478c4c5bded4f7bbd254efe7077e98fbefac6
Sabina <109144680+Srasulova@users.noreply.github.com>
2023-06-06 00:00:29 -0400

    add confirm-order file

---
+++ login-form/src/pages/confirm-order.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+type ConfirmOrderProps = {
+  showModal: boolean;
+  setShowModal: (value: boolean) => void;
+  name: string;
+};
+
+export default function ConfirmOrder({
+  showModal,
+  setShowModal,
+}: ConfirmOrderProps) {
+  return (
+    <div
+      className={`modal fade ${showModal ? "show" : ""}`}
+      id="exampleModal"
+      tabIndex={-1}
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+      style={{ display: showModal ? "block" : "none" }}
+    >
+      <div className="modal-dialog modal-dialog-centered text-success">
+        <div className="modal-content">
+          <div className="modal-header d-flex">
+            <h5
+              className="modal-title fs-5 text-center fw-bold text-success col-11"
+              id="exampleModalLabel"
+            >
+              Order details
+            </h5>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+              onClick={() => setShowModal(false)}
+            ></button>
+          </div>
+          <div className="modal-body">
+            <p>{name!}</p>
+          </div>
+          <div className="modal-footer">
+            {/* <button
+              type="button"
+              className="btn btn-secondary"
+              data-bs-dismiss="modal"
+              onClick={() => setShowModal(false)}
+            >
+              Close
+            </button> */}
+            <button type="button" className="btn btn-success mx-auto">
+              Confirm your order
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
